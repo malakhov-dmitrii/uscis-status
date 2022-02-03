3f0ff6c9426c060d244f6179aaa94da89a8a552e
Dmitriy Malakhov <mitia2022@gmail.com>
2022-02-03 12:30:44 +0300

    init

---
+++ index.js
@@ -0,0 +1,52 @@
+const fastify = require('fastify')({ logger: true });
+const puppeteer = require('puppeteer');
+const TeleBot = require('telebot');
+
+const NUMBER = 'WAC2208151046';
+const TELEGRAM_BOT_TOKEN = '1717140057:AAEsfxVO9GRl-yGc_uZdX0-QIIX9yWXG8hc';
+const TELEGRAM_CHAT_ID = '264414372';
+
+const bot = new TeleBot({ token: TELEGRAM_BOT_TOKEN });
+
+const main = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://egov.uscis.gov/casestatus/landing.do');
+
+  // find the form input by id "receipt_number"
+  await page.type('#receipt_number', NUMBER);
+
+  // find  the input with type 'submit'
+  await page.click('input[type="submit"]');
+
+  // wait for the page to load
+  await page.waitForNavigation();
+
+  // make a screenshot
+  await page.screenshot({ path: 'example.png' });
+
+  // find the value of h1
+  const text = await page.$eval('h1', el => el.innerText);
+  await browser.close();
+
+  return text;
+};
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  const text = await main();
+  bot.sendMessage(TELEGRAM_CHAT_ID, text);
+
+  return { status: text };
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
