65fb190f80ac830306b30ffd378b169b97179a19
Dmitriy Malakhov <mitia2022@gmail.com>
2022-08-08 15:41:03 +0700

    added capdef

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ fastify.get('/api/check', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(process.env.PORT);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
