9b33f29207aee05e2cdff55c83f89ff7f533cbd9
Dmitriy Malakhov <mitia2022@gmail.com>
2022-08-29 10:19:58 +0700

    test

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT, host: process.env.HOST || '0.0.0.0' });
+    await fastify.listen({ port: process.env.PORT, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
